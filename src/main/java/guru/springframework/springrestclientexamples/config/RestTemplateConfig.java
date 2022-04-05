eb58e5288100c6620ab7303e09483a68b7c4bcf3
Evan Fredericks <evan.fredericks@gmail.com>
2022-04-05 10:05:53 -0500

    webflux display data

--- src/main/java/guru/springframework/springrestclientexamples/config/RestTemplateConfig.java
+++ src/main/java/guru/springframework/springrestclientexamples/config/RestTemplateConfig.java
@@ -4,6 +4,11 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.filter.reactive.HiddenHttpMethodFilter;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilterChain;
+
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class RestTemplateConfig {
@@ -12,5 +17,15 @@ public class RestTemplateConfig {
     public RestTemplate restTemplate(RestTemplateBuilder builder){
 
         return builder.build();
+    }
+
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter() {
+            @Override
+            public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+                return chain.filter(exchange);
+            }
+        };
     }
 }
