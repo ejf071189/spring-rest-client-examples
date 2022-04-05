4c469224531e87a9e64d52f1ebf4f2dd1655de55
Evan Fredericks <evan.fredericks@gmail.com>
2022-04-05 10:20:30 -0500

    uri components builder

--- src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
+++ src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
@@ -4,8 +4,10 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import guru.springframework.api.domain.User;
 
@@ -13,15 +15,21 @@ import guru.springframework.api.domain.User;
 public class ApiServiceImpl implements  ApiService {
 
     private RestTemplate restTemplate;
+    private final String api_url;
 
-    public ApiServiceImpl(RestTemplate restTemplate) {
+    public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String api_url) {
         this.restTemplate = restTemplate;
+        this.api_url = api_url;
     }
 
     @Override
     public List<User> getUsers(Integer limit) {
 
-        User[] users = restTemplate.getForEntity("http://jsonplaceholder.typicode.com/users?_limit=" + limit, User[].class).getBody();
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder
+                .fromUriString(api_url)
+                .queryParam("_limit", limit);
+
+        User[] users = restTemplate.getForEntity(uriBuilder.toUriString(), User[].class).getBody();
 
         return Arrays.stream(users).collect(Collectors.toList());
     }
